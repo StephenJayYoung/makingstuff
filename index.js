bee0233d34f9ba15d44d4a691d3d807dbdd8f9bd
Stephen Young <StephenJayYoung@gmail.com>
2014-09-25 23:12:55 -0700

    started working on a map function, unfinished

---
+++ index.js
@@ -0,0 +1,33 @@
+var fish = ['one fish', 'two fish', 'red fish', 'bluefish'];
+var catchAFish = function () {
+    name = 'bluefish',
+    size = "soooo big",
+}
+
+var fishes = fish.map(catchAFish(fish) {
+    return fish + "es";
+
+});
+
+console.log(fishes);
+
+
+
+
+
+
+// var awesomeWords = ['mercurial', 'tenuous', 'hippopotomonstrosesquipedalian', ];
+
+// module.exports.map = function (array, fn) {
+// }
+
+
+
+
+
+
+// var coloredFishes = _.filter(-)
+
+
+// var numbers = [1, 2, 3, 4, 5, 6];
+// var evenNumbers = _.filter(numbers, function(n) { return (n % 2) === 0; }
